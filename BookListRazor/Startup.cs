c5bf63680392981a28beb8f61f084845b043db60
Viktor <viktor.v.antoniuk@gmail.com>
2021-04-06 16:59:36 +0300

    Created book model; Created context and DB migrated;

--- BookListRazor/Startup.cs
+++ BookListRazor/Startup.cs
@@ -8,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookListRazor.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookListRazor
 {
@@ -23,6 +25,7 @@ namespace BookListRazor
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(option => option.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddRazorPages();
         }
 
