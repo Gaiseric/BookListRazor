c88f70230c83aa2d969da260733a05f1934519e1
Gaiseric <gaisericvandalorum@gmail.com>
2021-05-18 16:12:03 +0300

    Common list page added

--- BookListRazor/Pages/BookList/List.cshtml.cs
+++ BookListRazor/Pages/BookList/List.cshtml.cs
@@ -4,13 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using BookListRazor.Models;
-using Microsoft.EntityFrameworkCore;
 using BookListRazor.Extensions;
 
 namespace BookListRazor.Pages.BookList
 {
-    public class ReadedModel : PageModel
+    public class ListModel : PageModel
     {
         public string AntiforgeryToken => HttpContext.GetAntiforgeryTokenForJs();
 
